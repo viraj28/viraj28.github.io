d4ec36f2336720f824292b9e9a4af0b2373e807c
Viraj Nirbhavane <vnirbhavane28@gmail.com>
2020-04-17 11:21:36 +0530

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,24 +4,24 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
-let showMenu = false;
+let showMenu  = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-  if(!showMenu) {
+function toggleMenu(){
+  if(!showMenu){
     hamburger.classList.add('open');
     nav.classList.add('open');
     menuNav.classList.add('open');
     navItems.forEach(item => item.classList.add('open'));
-
     showMenu = true;
-  } else {
+  }
+  else{
     hamburger.classList.remove('open');
     nav.classList.remove('open');
     menuNav.classList.remove('open');
     navItems.forEach(item => item.classList.remove('open'));
-
+    
     showMenu = false;
   }
 }
